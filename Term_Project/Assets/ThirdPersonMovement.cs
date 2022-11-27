0bf0eb21f7896c3470d615dc672c2a93e3e5b29a
mahir <20200808034@ogr.akdeniz.edu.tr>
2022-11-27 21:48:11 +0300

    last

--- Term_Project/Assets/ThirdPersonMovement.cs
+++ Term_Project/Assets/ThirdPersonMovement.cs
@@ -19,6 +19,9 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        if(!InBounds())
+           speed = 0f;
+
         if(direction.magnitude >= 0.1f) 
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -29,5 +32,13 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+
+    bool InBounds() 
+    {
+        if (Mathf.Sqrt((transform.position.x)*(transform.position.x) + (transform.position.z)*(transform.position.z)) >= 15)
+           return false;
+        return true;
+        
     }
 }
