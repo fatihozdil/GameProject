ae1990718a15da2915df7319eaedd41aecb440a6
Emre Gulbay <emregulbay3@gmail.com>
2022-11-28 12:42:32 +0300

    dead animation added & don't let player to move when it is dead

--- Term_Project/Assets/AniCons.cs
+++ Term_Project/Assets/AniCons.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class AniCons : MonoBehaviour
 {
     private Animator animator;
-    private float speed;
-
+    public float health=100f;
+     ThirdPersonMovement tps;
     void Start()
     {
         animator = GetComponent<Animator>();
+        tps=GetComponent<ThirdPersonMovement>();
+
     }
     
 
     // Update is called once per frame
     void Update() {
-    if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.K)){
+            health=0;
+        }
+    if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)&& health>0)
         animator.SetBool("walk", true);
     else
         animator.SetBool("walk", false);
-        
+    if(health<=0)
+        animator.SetBool("death",true); 
+
     }
 }
