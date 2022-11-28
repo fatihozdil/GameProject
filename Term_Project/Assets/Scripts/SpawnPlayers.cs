2bbcb94f6f201d02253957f7665c55f4f0b00a88
Fatih Ozdil <muhammedfatihozdil@gmail.com>
2022-11-29 02:05:13 +0300

    add prefab for player connect it to the photon

---
+++ Term_Project/Assets/Scripts/SpawnPlayers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //float randomX = Random.Range(minX, maxX);
+        //float randomZ = Random.Range(minZ, maxZ);
+        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0, 40, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
