174aa1498eb4efadb4a7a29741a684ce68fa744c
Ahmet Can Dericioglu <4hcan2001@gmail.com>
2022-12-15 22:13:48 +0300

    In game menu create

---
+++ Term_Project/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    
+    public GameObject inGameObject;
+    private bool isOpen;
+
+    void Start()
+    {
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            closeTab();
+        }        
+        inGameObject.SetActive(isOpen);
+    }
+
+    public void closeTab(){
+        isOpen = !isOpen;
+    }
+
+    public void BackToMenu(){
+        SceneManager.LoadScene(0);
+        PhotonNetwork.Disconnect();
+    }
+    public void BackToLobby(){
+        SceneManager.LoadScene(2);
+    }
+}
