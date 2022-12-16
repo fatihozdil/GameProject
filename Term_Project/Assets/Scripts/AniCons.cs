cb8779fdc12e4c8dc1be161d03684d63e30de684
Ahmet Can Dericioglu <4hcan2001@gmail.com>
2022-12-16 14:39:11 +0300

    dead animation fix

--- Term_Project/Assets/Scripts/AniCons.cs
+++ Term_Project/Assets/Scripts/AniCons.cs
@@ -16,7 +16,6 @@ public class AniCons : MonoBehaviour
         playerStats = GetComponent<PlayerStats>();
         animator = GetComponent<Animator>();
         photonView = GetComponent<PhotonView>();
-        playerHealth = playerStats.health;
     }
 
 
@@ -26,21 +25,21 @@ public class AniCons : MonoBehaviour
         
         if (photonView.IsMine)
         {
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) && playerHealth > 0)
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) && playerStats.health > 0)
                 animator.SetBool("walk", true);
             else
                 animator.SetBool("walk", false);
             //repeadet execution
-            if (Input.GetKey(KeyCode.K) || playerHealth == 0)
+            if (Input.GetKey(KeyCode.K) || playerStats.health == 0)
             {
                 animator.SetBool("isDead", true);
                 if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.5f && !animator.IsInTransition(0)) 
-                   playerHealth -= 100;
+                   playerStats.health -= 100;
             }
             else
                 animator.SetBool("isDead", false);
 
-            if (Input.GetButtonDown("Jump") && playerHealth > 0)
+            if (Input.GetButtonDown("Jump") && playerStats.health > 0)
             {
                 if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Male Jump Up") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Male Fall") &&
                 !animator.GetCurrentAnimatorStateInfo(0).IsName("Male Land"))
@@ -49,7 +48,7 @@ public class AniCons : MonoBehaviour
             else
                 animator.SetBool("jump", false);
 
-            if (Input.GetKey(KeyCode.Q) && playerHealth > 0)
+            if (Input.GetKey(KeyCode.Q) && playerStats.health > 0)
             {
                 if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Male Roll") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Male Idle 0"))
                 {
